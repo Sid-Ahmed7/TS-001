194dfd1e15d7f4eace9753ae46e4e53886ad4f83
Sid-Ahmed7 <sido91080@hotmail.fr>
2024-05-02 16:15:15 +0200

    Add TS-001

---
+++ build/index.js
@@ -0,0 +1,12 @@
+/* Transformer du code JavaScript en code Type Script */
+function hello(name) {
+    console.log("Hello " + name);
+}
+var firstName = "bob";
+hello(firstName);
+hello(firstName + " marley");
+function concat(a, b) {
+    return a + b;
+}
+var wcs = concat("Wild", concat("Code", "School"));
+console.log(wcs);
---
+++ src/index.ts
@@ -0,0 +1,17 @@
+/* Transformer du code JavaScript en code Type Script */
+
+function hello(name :string) : void {
+    console.log("Hello " + name);
+}
+
+const firstName : string = "bob";
+
+hello(firstName);
+hello(firstName + " marley");
+
+function concat(a:string, b:string) : string {
+    return a + b;
+}
+
+const wcs : string  = concat("Wild", concat("Code", "School"));
+console.log(wcs);
